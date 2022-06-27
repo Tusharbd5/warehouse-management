0cc61defbb031f2ac9fd3e7933b6f9cd2ccb9872
Tusharbd5 <tushardebhata24@gmail.com>
2022-06-27 23:20:06 +0600

    footer changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            <p className='text-white m-0'><small>copyright reserved by © self-teaching-provider</small></p>
+            <p className='text-white m-0'><small>copyright reserved by © warehouse management</small></p>
         </div>
     );
 };
